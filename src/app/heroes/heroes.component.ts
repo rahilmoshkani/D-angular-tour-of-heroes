47e4416315879c6aff9b94474be6baaf7678aa54
Rahele moshkani <rahilmoshkani@gmail.com>
2021-11-02 19:54:30 +0330

    hero:HERO={}

--- src/app/heroes/heroes.component.ts
+++ src/app/heroes/heroes.component.ts
@@ -1,4 +1,5 @@
 import { Component, OnInit } from '@angular/core';
+import { HERO } from '../HERO';
 
 @Component({
   selector: 'app-heroes',
@@ -7,7 +8,10 @@ import { Component, OnInit } from '@angular/core';
 })
 export class HeroesComponent implements OnInit {
 
-  hero='Windstorm';
+  hero:HERO={
+    id:1,
+    name:'Windstorm'
+     };
   constructor() { }
 
   ngOnInit(): void {
