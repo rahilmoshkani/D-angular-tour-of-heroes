e450386b604e4ce42ac8f098b933bc6c53dd00a9
Rahele moshkani <rahilmoshkani@gmail.com>
2021-11-07 19:31:01 +0330

    get hero data

--- src/app/hero.service.ts
+++ src/app/hero.service.ts
@@ -1,9 +1,13 @@
 import { Injectable } from '@angular/core';
-
+import { HEROES } from './mock-heroes';
+import { HERO } from './HERO';
 @Injectable({
   providedIn: 'root'
 })
 export class HeroService {
+  getheroes():HERO[]{
+    return HEROES;
+  }
 
   constructor() { }
 }
