033fa723d075669dbc8ec4fc66aa5248741e52d5
Rahele moshkani <rahilmoshkani@gmail.com>
2021-11-14 01:14:25 +0330

    replace defult file content in three files

--- src/app/dashboard/dashboard.component.ts
+++ src/app/dashboard/dashboard.component.ts
@@ -1,3 +1,5 @@
+import { HeroService } from './../hero.service';
+import { HERO } from './../hero';
 import { Component, OnInit } from '@angular/core';
 
 @Component({
@@ -6,10 +8,16 @@ import { Component, OnInit } from '@angular/core';
   styleUrls: ['./dashboard.component.scss']
 })
 export class DashboardComponent implements OnInit {
+  heroes:HERO[]=[];
 
-  constructor() { }
+  constructor(private heroservice:HeroService) { }
 
-  ngOnInit(): void {
+  ngOnInit(){
+    this.getHeroes();
+  }
+  getHeroes():void{
+    this.heroservice.getheroes()
+    .subscribe(heroes=>this.heroes=heroes.slice(1,5));
   }
 
 }
