0326a35738eb72743de23f9ed0fcaa85ee35445a
Rahele moshkani <rahilmoshkani@gmail.com>
2021-11-14 09:38:36 +0330

    add two routes

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,10 +1,12 @@
 import { HeroesComponent } from './heroes/heroes.component';
 import { Routes, RouterModule } from '@angular/router';
 import { NgModule } from '@angular/core';
-
+import { DashboardComponent } from './dashboard/dashboard.component';
 
 const routes:Routes=[
-  {path:'heroes',component:HeroesComponent}
+  {path:'heroes',component:HeroesComponent},
+  {path:'dashboard',component:DashboardComponent},
+  {path:'',redirectTo:'/dashboard',pathMatch:'full'}
 ];
 
 @NgModule({
