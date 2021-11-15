6688498992447c80e32003e41f57b45b92b81a90
Rahele moshkani <rahilmoshkani@gmail.com>
2021-11-15 15:49:11 +0330

    debugging

--- src/app/in-memory-data.service.ts
+++ src/app/in-memory-data.service.ts
@@ -25,7 +25,7 @@ export class InMemoryDataService implements InMemoryDbService {
     genId(heroes:HERO[]):Number{
       return heroes.length>0 ? Math.max(...heroes.map(hero=>hero.id))+1:11;
     }
-  }
+  
 
   constructor() { }
 }
